48519624dbc88388fd9fe0997ef017cacc9f1f08
Daniel <itzdanielmichael@gmail.com>
2021-11-27 11:59:50 -0500

    create Input

--- jotto/client/src/components/GuessedWords.jsx
+++ jotto/client/src/components/GuessedWords.jsx
@@ -1,7 +1,9 @@
 import { useMemo } from 'react';
 import PropTypes from 'prop-types';
 
-const TABLE_CELL_CLASS = 'px-8 py-4 text-base text-bold text-gray-500';
+const TABLE_CELL_CLASS =
+  'px-8 py-4 text-base text-bold text-gray-500 text-left border border-grey-500';
+
 export default function GuessedWords({ guessedWords }) {
   const contentsJSX = useMemo(() => {
     if (!guessedWords.length) {
@@ -23,11 +25,15 @@ export default function GuessedWords({ guessedWords }) {
       return (
         <div data-test="guessed-words">
           <h3 className="text-3xl mx-2 mt-4 mb-2 text-black">Guessed Words</h3>
-          <table className="table-auto border-b border-t border-gray-200 shadow">
+          <table className="table-fixed border-b border-t border-gray-200 shadow w-full">
             <thead className="bg-gray-50">
               <tr className="whitespace-nowrap">
-                <th className={TABLE_CELL_CLASS}>Guess</th>
-                <th className={TABLE_CELL_CLASS}>Matching Letters</th>
+                <th scope="col" className={TABLE_CELL_CLASS}>
+                  Guess
+                </th>
+                <th scope="col" className={TABLE_CELL_CLASS}>
+                  Matching Letters
+                </th>
               </tr>
             </thead>
 
--- jotto/client/src/components/Input.jsx
+++ jotto/client/src/components/Input.jsx
@@ -1,6 +1,43 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 export default function Input({ secretWord }) {
-  return <div data-test="component-input"></div>;
+  const [currentGuess, setCurrentGuess] = useState('');
+
+  return (
+    <div data-test="component-input">
+      <form className="w-full max-w-sm">
+        <div class="md:flex md:items-center mb-6">
+          <div class="mb-4">
+            <label
+              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+              htmlFor="currentGuess">
+              Guess
+            </label>
+            <input
+              value={currentGuess}
+              onChange={(e) => setCurrentGuess(e.target.value)}
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              name="currentGuess"
+              type="text"
+              placeholder="Guess"
+            />
+          </div>
+        </div>
+
+        <div class="md:flex md:items-center">
+          <div class="md:w-1/3"></div>
+          <div class="md:w-2/3">
+            <button
+              data-test="submit-button"
+              class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              type="button">
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 Input.propTypes = {
